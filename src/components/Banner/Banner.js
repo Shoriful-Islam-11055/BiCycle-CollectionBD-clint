f97586551071e269790a8202838243046facb762
Shoriful Islam <shoriful15-11055@diu.edu.bd>
2022-04-29 23:58:47 +0600

    logo add, navbar and banner tanplete add

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import './Banner.css'
+
+import banner1 from '../../images/banner/banner1_2_1440x1080.jpg'
+
+const Banner = () => {
+  return (
+    <div>
+      <Carousel fade>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 banner-image"
+            src={banner1}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 banner-image"
+            src={banner1}
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 banner-image"
+            src={banner1}
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Banner;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,33 @@
 import React from "react";
-import { Container, Nav, Navbar} from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import band_logo from "../../images/logos/cycle-logo111.png";
 
 const Header = () => {
   return (
     <div>
       <Navbar bg="dark" variant="dark" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">
+            <img
+              src={band_logo}
+              width="150"
+              height="70"
+              className="d-inline-block align-top rounded-3"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
+              <div className="navbar-nav">
+                <Link className="nav-link active" aria-current="page" to="/">
+                  HOME
+                </Link>
+                <Link className="nav-link" to="/services">
+                  SERVICES
+                </Link>
+              </div>
             </Nav>
           </Navbar.Collapse>
         </Container>
