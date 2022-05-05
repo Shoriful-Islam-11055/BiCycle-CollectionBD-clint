714fc5ce2ddc488c95e182d2d77ef68c7155a629
Shoriful Islam <shoriful15-11055@diu.edu.bd>
2022-05-05 17:04:58 +0600

    login from full design

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,6 @@ import "./Footer.css";
 import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee, faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import {
   faFacebookSquare,
   faInstagramSquare,
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,18 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
 import { Button, Col, Form, InputGroup, Row } from "react-bootstrap";
-import './Login.css'
+import {
+  faGithub,
+  faGooglePlusSquare,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faUser,
+  faKey,
+  faEnvelope,
+  faRegistered,
+} from "@fortawesome/free-solid-svg-icons";
+import "./Login.css";
+import or_sign from "../../images/others/or.PNG";
 
 const Login = () => {
   const [validated, setValidated] = useState(false);
@@ -15,64 +27,118 @@ const Login = () => {
     setValidated(true);
   };
   return (
-    <div className="bg-success vh-100">
-      <Form noValidate validated={validated} onSubmit={handleSubmit} className = 'form-layout'>
+    <div className="bg-success p-2">
+      <Form
+        noValidate
+        validated={validated}
+        onSubmit={handleSubmit}
+        className="form-layout"
+      >
         <div className="p-3">
-        <h2 className="text-center">LOGIN</h2>
-        <Row className="mb-3">
-          <Form.Group as={Col} md="10" controlId="validationCustomUsername" className="mx-auto ">
-            <Form.Label>Username</Form.Label>
-            <InputGroup hasValidation>
-              <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-              <Form.Control
-                type="text"
-                placeholder="Username"
-                aria-describedby="inputGroupPrepend"
+          <h2 className="text-center fw-bold">LOGIN</h2>
+          <Row className="mb-3">
+            <Form.Group
+              as={Col}
+              md="10"
+              controlId="validationCustomUsername"
+              className="mx-auto "
+            >
+              <Form.Label>Username</Form.Label>
+              <InputGroup hasValidation>
+                <InputGroup.Text id="inputGroupPrepend">
+                  <FontAwesomeIcon icon={faUser} className="fs-3" />
+                </InputGroup.Text>
+                <Form.Control
+                  type="text"
+                  placeholder="Username"
+                  aria-describedby="inputGroupPrepend"
+                  required
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please choose a username.
+                </Form.Control.Feedback>
+              </InputGroup>
+            </Form.Group>
+          </Row>
+
+          <Row className="mb-3">
+            <Form.Group
+              as={Col}
+              md="10"
+              controlId="validationCustomUsername"
+              className="mx-auto"
+            >
+              <Form.Label>User Password</Form.Label>
+              <InputGroup hasValidation>
+                <InputGroup.Text id="inputGroupPrepend">
+                  <FontAwesomeIcon icon={faKey} className="fs-3" />
+                </InputGroup.Text>
+                <Form.Control
+                  type="Password"
+                  placeholder="Password"
+                  aria-describedby="inputGroupPrepend"
+                  required
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please enter valid password.
+                </Form.Control.Feedback>
+              </InputGroup>
+            </Form.Group>
+          </Row>
+
+          <Row className="mb-3">
+            <Form.Group as={Col} md="10" className="mx-auto">
+              <Form.Check
                 required
+                label="Agree to terms and conditions"
+                feedback="You must agree before submitting."
+                feedbackType="invalid"
               />
-              <Form.Control.Feedback type="invalid">
-                Please choose a username.
-              </Form.Control.Feedback>
-            </InputGroup>
-          </Form.Group>
-        </Row>
+            </Form.Group>
+          </Row>
 
-        <Row className="mb-3">
-          <Form.Group as={Col} md="10" controlId="validationCustomUsername" className="mx-auto">
-            <Form.Label>Username</Form.Label>
-            <InputGroup hasValidation>
-              <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-              <Form.Control
-                type="text"
-                placeholder="Username"
-                aria-describedby="inputGroupPrepend"
-                required
-              />
-              <Form.Control.Feedback type="invalid">
-                Please choose a username.
-              </Form.Control.Feedback>
-            </InputGroup>
-          </Form.Group>
-        </Row>
-
-        <Row className="mb-3">
-          <Form.Group as={Col} md="10" className="mx-auto">
-          <Form.Check
-            required
-            label="Agree to terms and conditions"
-            feedback="You must agree before submitting."
-            feedbackType="invalid"
-          /> 
-          </Form.Group>
-        </Row>
-
-        <Row className="mb-3">
-          <Form.Group as={Col} md="10" className="mx-auto">
-          <Button type="submit">Submit form</Button>
-          </Form.Group>
-        </Row>
+          <div className="text-center">
+            <Button className="bg-success px-5 fs-5" type="submit">
+              Login
+            </Button>
+          </div>
         </div>
       </Form>
+      <div className="form-layout-down">
+        <div className="text-center">
+          <p className="fs-3">or</p>
+          <p className="fs-4 fw-bold m-0">Continue</p>
+          <p className="fs-5 fw-bold m-0">With your social media account</p>
+        </div>
+        <div className="text-center">
+          <Button className="fs-5 fw-bold me-2">
+            <FontAwesomeIcon icon={faGooglePlusSquare} className="fs-4" />
+            <span className="ms-1">Google</span>
+          </Button>
+
+          <Button className="fs-5 fw-bold me-2 bg-success">
+            <FontAwesomeIcon icon={faEnvelope} className="fs-4" />
+            <span className="ms-1">Email</span>
+          </Button>
+
+          <Button className="fs-5 fw-bold bg-info git-btn">
+            <FontAwesomeIcon icon={faGithub} className="fs-4" />
+            <span className="ms-1">GitHub</span>
+          </Button>
+        </div>
+        <hr className="w-75 mx-auto" />
+
+        <div className="text-center pb-2">
+          <p className="text-bold fs-5">
+            Don't have an account?
+            <span className="text-danger">Register Now!</span>
+          </p>
+          <Button className="fs-5 fw-bold bg-info git-btn">
+            <FontAwesomeIcon icon={faRegistered} className="fs-4" />
+            <span className="ms-1">Register</span>
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
