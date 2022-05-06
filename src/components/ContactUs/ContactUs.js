e24e1e5db1b79f9f609e52d4a194a4b69b74d2e1
Shoriful Islam <shoriful15-11055@diu.edu.bd>
2022-05-06 12:31:38 +0600

    contact us page complete

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,30 +1,93 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Button, Col, Form, Row } from "react-bootstrap";
+import "./ContactUs.css";
 const ContactUs = () => {
-  return (
-    <div className="container">
-      <h2 className="section-title text-center py-5">
-        Contact us for more information
-      </h2>
-      <div class="card mb-3 border-0">
-        <div class="row g-0">
-          
-          <div class="col-md-6">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-              <p class="card-text">
-                <small class="text-muted">Last updated 3 mins ago</small>
-              </p>
-            </div>
-          </div>
+  const [validated, setValidated] = useState(false);
 
-          <div class="col-md-6">
-            
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+  return (
+    <div className="container background-info p-3 mb-3 mt-3">
+      <p className="section-title text-center py-3 mt-3 fs-2 background-info">
+        Contact us for more information
+      </p>
+      <div class="card mb-3 border-0 p-3 bg-light">
+        <div>
+          <div class="row g-0">
+            <div class="col-md-6">
+              <div class="card-body">
+                <h5 class="card-title">Stay with us and know updated</h5>
+                <p class="card-text">
+                  Collecting various domestic and foreign bicycles is our hobby
+                  and profession. Every week at our Crazy Collection Warehouse
+                  we import high quality and popular bicycles from abroad and
+                  the bicycles are made in our own factory. We export our
+                  manufactured bicycles to different countries through different
+                  agents. Bicycles made by us and bicycles imported from abroad
+                  are distributed in various reputed bicycle showrooms of the
+                  country. Stay tuned for updates on new collections every week.
+                  To find out more about us, fill out the form and submit. We
+                  will tell you more details about us. Thank you.
+                </p>
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                <Row className="mb-3">
+                  <Form.Group as={Col} md="10" controlId="validationCustom03">
+                    <Form.Control
+                      className="p-3"
+                      type="text"
+                      placeholder="User Name"
+                      required
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Please provide a valid Username.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </Row>
+                <Row className="mb-3">
+                  <Form.Group as={Col} md="10" controlId="validationCustom03">
+                    <Form.Control
+                      className="p-3"
+                      type="text"
+                      placeholder="Your Address"
+                      required
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Please provide a valid Address.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </Row>
+                <Row className="mb-3">
+                  <Form.Group as={Col} md="10" controlId="validationCustom03">
+                    <Form.Control
+                      as="textarea"
+                      rows={3}
+                      placeholder="Please provide feedback about us"
+                      required
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Please provide feedback.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </Row>
+
+                <div className="text-center Subscribe">
+                  <Button variant="outline-dark fs-5 w-100 p-2" type="submit">
+                    Subscribe
+                  </Button>
+                </div>
+              </Form>
+            </div>
           </div>
         </div>
       </div>
